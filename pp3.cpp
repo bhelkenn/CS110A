038f6392c9b0660f04fb8c6483dcf4fc6385cde4
unknown <bhelkenn@US-DK-00189.plos.org>
2014-06-05 15:39:32 -0700

    first commit

---
+++ pp3.cpp
@@ -0,0 +1,19 @@
+//Script started on Fri Sep 9 19:07:29 2011
+//Brady Helkenn
+//pp3.cpp
+//CS 110A Practice Problem 3
+#include <iostream>
+using namespace std;
+int main() {	int GallonsOfGas = 12;
+	int MilesTraveled = 350;
+	int MPG;
+	
+	// Calculate the miles per gallon
+	MPG = MilesTraveled / GallonsOfGas;
+	
+	cout << "A vehicle with a " << GallonsOfGas << " gallon tank that ";
+	cout << "travels " << MilesTraveled << " miles on a full tank has ";
+	cout << "an MPG rating of " << MPG << "." << endl;
+	
+	return 0;
+}
